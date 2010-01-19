9895090af3343391a21a9d89f720d3c66f890e13
eartola12 <eartola12@6c91f584-e5c6-11de-9298-fbd6a473cdd1>
2010-01-19 18:25:56 +0000

    experiment on classification

--- experiment.m
+++ experiment.m
@@ -1,21 +1,48 @@
-function [ output_args ] = experiment( input_args )
+function [ confusionMatrizes ] = experiment(K)
 %EXPERIMENT Summary of this function goes here
 %   Detailed explanation goes here
 
 % Load images (each row is an image) DONE BY LLUIS AND EKAIN
+[faces, faceLabels] = imageOnMatrix();
 
+faces=double(faces);
 % Split data in Train and Test (CROSSVALIND)
+indices = CROSSVALIND('Kfold',faceLabels,K);
+confusionMatrizes= cell(K,1);
+% TRAINING
+for i=1:K
+    %cp = classperf(faceLabels);
+    test = (indices == i);
+    train = ~test;
+    %   Eigenfaces decomposition (eigenvectors num choice)
+    eigenfaces = getEigenFaces(double(faces(train,:)'), length(train));
+    
+    %  Training Data projection
+    projectedTrainingFaces = faces(train,:) * eigenfaces;
+    
+    %   Test data projection
+    projectedTestFaces = faces(test,:) * eigenfaces;
+    
+    %   In the case of SVM, train the classifier
+    
+    %   Classification
+    classes = KNNCLASSIFY(projectedTestFaces,projectedTrainingFaces,faceLabels(train),3);
+    
+    %   Evaluation performance (CLASSPERF)
+    cp=classperf(faceLabels(test),classes);
+    
+    confusionMatrizes{i} = cp.CountingMatrix;
+end
 
-% TRAINING: DONE BY AHMED
-%   Eigenfaces decomposition (eigenvectors num choice)
-%   Data projection
-%   In the case of SVM, train the classifier
+
+
+
 
 % TEST
 %   Data projection
-%   Classification
 
-% Evaluation performance (CLASSPERF)
+
+%
 
 % Repeat experiment with different parameters and build ROC curve DONE BY
 % TONI
--- getEigenFaces.m
+++ getEigenFaces.m
@@ -7,19 +7,22 @@ function [eigen_faces] = getEigenFaces(original_faces, evecs_count)
   % Get top 30 PC evectors of Imgs
   [Vecs, Vals, Psi] = pc_evectors(original_faces, evecs_count);
 
-%   plot(Vals); % To plot the eigenvalues
-%   CVals = zeros(1,length(Vals)); % Allocate a vector same length as Vals
-%   CVals(1) = Vals(1);
-% 
-%   for i = 2:length(Vals) % Accumulate the eigenvalue sum
-%     CVals(i) = CVals(i-1) + Vals(i);
-%   end;
-% 
-%   CVals = CVals / sum(Vals); % Normalize total sum to 1.0
-%   plot(CVals); % Plot the cumulative sum
-%   ylim([0 1]); % Set Y-axis limits to be 0-1
+  plot(Vals); % To plot the eigenvalues
+  CVals = zeros(1,length(Vals)); % Allocate a vector same length as Vals
+  CVals(1) = Vals(1);
 
-  save eigenfaces.mat Vecs;
+  for i = 2:length(Vals) % Accumulate the eigenvalue sum
+    CVals(i) = CVals(i-1) + Vals(i);
+  end;
+
+  CVals = CVals / sum(Vals); % Normalize total sum to 1.0
+  plot(CVals); % Plot the cumulative sum
+  ylim([0 1]); % Set Y-axis limits to be 0-1
+  
+  num_evecs = input('Number of eigenvectors? ');
+
+  eigen_faces = Vecs(:,1:num_evecs);
+%   save eigenfaces.mat Vecs;
 
 end
 
