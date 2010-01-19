2b633dcb188c628e55f4b05e319f9943c29344c1
a.mounir86 <a.mounir86@6c91f584-e5c6-11de-9298-fbd6a473cdd1>
2010-01-19 12:48:22 +0000

    trainClassifier trains the classifier using the OAA strategy chooseTrainingData chooses some of the data for training and the rest are for testing (implementation not finished yet)

---
+++ chooseTrainingData.m
@@ -0,0 +1,25 @@
+function [tFaces, tLabels] = chooseTrainingData(allFaces, allLabels)
+% ChooseTrainingData, this function chooses some data to be the training
+% data of the system.
+%
+% INPUT: allFaces, a variable containing all of the faces that exist in the
+%                  sytem.
+%        allLabels, the labels of the faces of the system.
+%
+% OUTPUT: tFaces, the training faces for the classifier.
+%         tLabels, the labels of the training faces.
+
+    % Prepare the eigen faces array
+    
+    
+    % Display the plot to the user
+    
+    
+    % Read the value of the user requires for the eigen faces
+    
+    
+    % Return the top user selected value eigen faces
+
+
+end
+
