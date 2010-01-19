5101fec4f918071f09c591c064c11d884a4fd231
chester66 <chester66@6c91f584-e5c6-11de-9298-fbd6a473cdd1>
2010-01-19 10:38:44 +0000

    workflow, assigned tasks for each one of us

---
+++ experiment.m
@@ -0,0 +1,25 @@
+function [ output_args ] = experiment( input_args )
+%EXPERIMENT Summary of this function goes here
+%   Detailed explanation goes here
+
+% Load images (each row is an image) DONE BY LLUIS AND EKAIN
+
+% Split data in Train and Test (CROSSVALIND)
+
+% TRAINING: DONE BY AHMED
+%   Eigenfaces decomposition (eigenvectors num choice)
+%   Data projection
+%   In the case of SVM, train the classifier
+
+% TEST
+%   Data projection
+%   Classification
+
+% Evaluation performance (CLASSPERF)
+
+% Repeat experiment with different parameters and build ROC curve DONE BY
+% TONI
+
+
+end
+
